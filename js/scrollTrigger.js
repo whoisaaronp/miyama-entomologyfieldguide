170546c5afd9f1a615cf27c373b45f455492594d
Aaron Paterson <juniha.aaron@gmail.com>
2020-11-13 16:57:57 -0500

    working on infantry page

---
+++ js/scrollTrigger.js
@@ -0,0 +1,12 @@
+//  scroll magic niceness
+
+
+(function () {
+    
+    "use strict"
+    
+    gsap.to("#vertical", 1, {delay: .5, y:600, ease:Power2.easeOut});
+    console.log("fired");
+
+    
+});
