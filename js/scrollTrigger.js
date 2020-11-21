9b22036fe24c1d98db38f437fdc118b80afcf882
whoisaaronp <juniha.aaron@gmail.com>
2020-11-20 20:04:30 -0500

    added the full width background image fix

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -6,8 +6,8 @@
     gsap.registerPlugin(ScrollTrigger);
     "use strict"
     // stag-beetle landing page
-    gsap.from("#vertical", 1, {autoAlpha: 0, delay: 0.5, y:800, ease:Expo.easeOut});
-    gsap.from("#delay-button", 1, {autoAlpha: 0, delay: 0.8, y:800, ease:Expo.easeOut});
+    gsap.from("#vertical", 1, {autoAlpha: 0, stagger: 0.2, delay: 0.5, y:800, ease:Expo.easeOut});
+    gsap.from("#delay-button", 1, {autoAlpha: 0, stagger: 0.2, delay: 0.8, y:800, ease:Expo.easeOut});
     // console.log("fired");
 
 })();
