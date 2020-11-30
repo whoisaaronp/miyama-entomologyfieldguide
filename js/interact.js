5d8fe548bc7d0d1b9eaaa5b190bdc0f2792e613b
whoisaaronp <juniha.aaron@gmail.com>
2020-11-30 16:26:48 -0500

    progress with toggle dots position js

--- js/interact.js
+++ js/interact.js
@@ -10,20 +10,28 @@
 
     function addContent1() {
         content.innerHTML = `
-    <p>The head, having mouthparts projecting forward or sometimes downturned, is usually heavily
-    sclerotized and is sometimes very large. The eyes are compound and may display remarkable adaptability..</p>
+        <h2 style="font-size: 24px; 
+        font-weight:800; margin-bottom: 15px;">Thorax</h2>
+    <p style="line-height: 1.5em; margin-bottom: 10px;">
+    The thorax is segmented into the two discernible parts, the pro- and pterothorax. The pterothorax is the fused meso- and metathorax, which are commonly separated in other insect species, although flexibly articulate from the prothorax. Shiny(middle section) and their wing cases are red chestnut brown. The thorax is attached head and has the three pairs of legs attached. All insects have 3 pairs of legs joined to the thorax. At the end of their legs, stag beetles have little hooks to help them grip on to things.
+    </p>
     `
     }
 
     function addContent2() {
-        content.innerHTML = `
-    <p>The forewings of beetles are not used for flight, but form elytra which cover the hind part of the body and protect the hindwings. The elytra are usually hard shell-like structures which must be raised to allow the hind wings to move for flight.</p>
+        content.innerHTML = `<h2 style="font-size: 24px; 
+        font-weight:800; margin-bottom: 15px;">Mandibles</h2>
+    <p style="line-height: 1.5em; margin-bottom: 10px;">
+    The head of the male has enlarged jaws or mandibles. These are used for fighting with other males, for attracting a mate, and for holding the mate still during mating. The stag beetle also uses his jaws to protect himself if he feels threatened.</p>
     `
     }
 
     function addContent3() {
-        content.innerHTML = `
-    <p>The thorax is segmented into the two discernible parts, the pro- and pterothorax. The pterothorax is the fused meso- and metathorax, which are commonly separated in other insect species, although flexibly articulate from the prothorax.</p>
+        content.innerHTML = `<h2 style="font-size: 24px; 
+        font-weight:800; margin-bottom: 15px;">Wings</h2>
+    <p style="line-height: 1.5em; margin-bottom: 10px;">
+    The stag beetle has two pairs of wings, like all insects. The top pair, also called elytra or wing cases, is very hard and shiny. These cover a fine, veined pair underneath which look like very large fly wings. When the stag beetle flies it holds the wing cases up and unfolds the soft wings underneath. It flies upright with its large black wing cases held behind it. It is also very noisy.
+    </p>
     `
     }
 
